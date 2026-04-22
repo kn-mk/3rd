f85dced67e648a71129030ac4488f36ecde59ee5
kn-mk <nishimurakenta.mk@gmail.com>
2026-04-22 16:08:38 +0900

    12-2-1

--- 3rd/meikai12-2/Account.java
+++ 3rd/meikai12-2/Account.java
@@ -1,1 +1,38 @@
 // 銀行口座クラス【第2版】Accountを書く
+class Account {
+	private String name;		// 口座名義
+	private String no;			// 口座番号
+	private long balance;		// 預金残高
+
+	//--- コンストラクタ ---//
+	Account(String n, String num, long z) {
+		name = n;				// 口座名義
+		no = num;				// 口座番号
+		balance = z;		// 預金残高
+	}
+
+	//--- 口座名義を調べる ---//
+	String getName() {
+		return name;
+	}
+
+	//--- 口座番号を調べる ---//
+	String getNo() {
+		return no;
+	}
+
+	//--- 預金残高を調べる ---//
+	long getBalance() {
+		return balance;
+	}
+
+	//--- k円預ける ---//
+	void deposit(long k) {
+		balance += k;
+	}
+
+	//--- k円おろす ---//
+	void withdraw(long k) {
+		balance -= k;
+	}
+}
--- 3rd/meikai12-2/TimeAccount.java
+++ 3rd/meikai12-2/TimeAccount.java
@@ -1,1 +1,22 @@
 // 定期預金付き銀行口座クラス【第1版】TimeAccountを書く
+
+class TimeAccount {
+	private long timeBalance;				// 預金残高（定期預金）
+
+	// コンストラクタ
+	TimeAccount(String name, String no, long balance, long timeBalance) {
+		super(name, no, balance);				// クラスAccountのコンストラクタの呼出し
+		this.timeBalance = timeBalance;	// 預金残高（定期預金）
+	}
+
+	// 定期預金残高を調べる
+	long getTimeBalance() {
+		return timeBalance;
+	}
+
+	// 定期預金を解約して全額を普通預金に移す
+	void cancel() {
+		deposit(timeBalance);
+		timeBalance = 0;
+	}
+}
