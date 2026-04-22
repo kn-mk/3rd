65c4264dcf9f92f911af66fa37bb075858f7b0b8
kn-mk <nishimurakenta.mk@gmail.com>
2026-04-22 15:22:24 +0900

    12-1-1メソッド？

--- 3rd/meikai12-1/OdometerCar.java
+++ 3rd/meikai12-1/OdometerCar.java
@@ -1,1 +1,31 @@
 // 派生クラスOdometerCarを書く
+public class OdometerCar extends Car {
+    private int mileage;
+
+    //コンストラクタ
+    OdometerCar(String name, int width, int height, int length, double fuel, Day purchaseDay, int mileage) {
+        super(name, width, height, length, fuel, purchaseDay);
+        this.mileage = mileage;
+    }
+
+    public boolean move(double dx, double dy) {
+		double dist = Math.sqrt(dx * dx + dy * dy);		// 移動距離
+
+		if (dist > this.getFuel())
+			return false;				// 移動できない　… 燃料不足
+		else {
+			fuel -= dist;				// 移動距離の分だけ燃料が減る
+			x += dx;
+			y += dy;
+            mileage += dist;
+			return true;				// 移動完了
+		}
+	}
+
+    public int getMileage() {
+        return mileage;
+    }
+
+
+
+}
