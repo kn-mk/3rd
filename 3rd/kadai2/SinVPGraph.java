98e6072c33b66e5e18ccd6c9ebd7860a5dbad79c
kn-mk <nishimurakenta.mk@gmail.com>
2026-05-11 23:14:05 +0900

    途中

--- 3rd/kadai2/SinVPGraph.java
+++ 3rd/kadai2/SinVPGraph.java
@@ -1,19 +1,21 @@
 //VirtPlotGraphのサブクラスのSinVPGraphクラスを書く
 
-public class SinVPGraph ???{
+public class SinVPGraph extends VirtPlotGraph{
 
 	protected int t;
 
-	public SinVPGraph(???, int t){
-		super(???);
-		??? = t;
+	public SinVPGraph(char symbol, int t){
+		super(symbol);
+		this.t = t;
 	}
 
-	public int getT(){ ??? }
-	public void setT(???){ ??? }
+	public int getT(){ return t; }
+	public void setT(int t){ this.t = t; }
 
 	@Override public void computeY(){
-		setY(???);
+		//振幅:Y_MAX
+		int y = (int)(Math.sin(2 * Math.PI * getX() / t) * Y_MAX);
+		setY(y);
 	}
 
 }
--- 3rd/kadai2/VirtPlotGraph.java
+++ 3rd/kadai2/VirtPlotGraph.java
@@ -7,31 +7,31 @@ public class VirtPlotGraph{
 
 	private static int x = 0;
 
-	private static char buffer[] = new char[??? - ??? + 1];
+	private static char buffer[] = new char[Y_MAX - Y_MIN + 1];
 	
-	public static int getX(){ ??? }
+	public static int getX(){ return x; }
 	public static void setX(int i){ x = i; } // thisは使えない
 
 	public static void clear(){
-		for(int y = 0;y < buffer.???;y++) buffer[???] = ' ';
+		for(int y = 0;y < buffer.length;y++) buffer[y] = ' ';
 	}
 
 	public static void println(){  System.out.println(new String(buffer)); }
 
 	public void plot(){
 		int y = this.y;
-		if(y < ???) y = Y_MIN;
-		if(??? > ???) ???;
-		buffer[y - ???] = symbol;
+		if(y < Y_MIN) y = Y_MIN;
+		if(y > Y_MAX) y = Y_MAX;
+		buffer[y - Y_MIN] = symbol;
 	}
 
 	private char symbol;	
 	private int y = 0;
 
-	public VirtPlotGraph(char symbol){ ??? = symbol; }
+	public VirtPlotGraph(char symbol){ this.symbol = symbol; }
 
-	public int getY(){ ??? }
-	public void setY(???){ ??? }
+	public int getY(){ return y; }
+	public void setY(int y){ this.y = y; }
 
 	public void computeY(){ setY(y); }
 
